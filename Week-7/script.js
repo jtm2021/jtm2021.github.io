958527031be1450ffde4de8769e11585fb920588
Jesson Mangilisan <jesson_chubz@yahoo.com.ph>
2024-11-03 12:00:06 +0000

    Customize final message styling

--- Week-7/script.js
+++ Week-7/script.js
@@ -1,7 +1,11 @@
 const cardContainer = document.querySelector('.form-container');
 const messageContainer = document.querySelector('.message');
-const finalMessage = document.querySelector('.final-message');
+const finalMessageOne = document.querySelector('.final-message-one');
+const finalMessageTwo = document.querySelector('.final-message-two');
+const finalUserName = document.querySelector('.final-user-name');
+const finalUserEmail = document.querySelector('.final-user-email');
 const form = document.querySelector('form');
+const span = document.querySelector('span');
 
 form.onsubmit = (e) => {
     e.preventDefault();
@@ -10,6 +14,8 @@ form.onsubmit = (e) => {
     console.log(userName + " " + userEmail);
     cardContainer.style.display = "none";
     messageContainer.style.display = "block";
-    finalMessage.textContent = userName + ", thank you for your details. We will be in touch via " + userEmail + " shortly.";
+    finalUserName.textContent = userName;
+    finalUserEmail.textContent = userEmail;
+    finalMessageOne.textContent = ", thank you for your details. We will be in touch via ";
+    finalMessageTwo.textContent = " shortly.";
 }
-
