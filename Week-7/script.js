936dada993fb000e2cccd317146cb0677ba16c1e
Jesson Mangilisan <jesson_chubz@yahoo.com.ph>
2024-11-03 11:40:39 +0000

    Prevent default when submitting the form

--- Week-7/script.js
+++ Week-7/script.js
@@ -1,9 +1,15 @@
-const cardContainer = document.querySelector('.card-container');
+const cardContainer = document.querySelector('.form-container');
+const messageContainer = document.querySelector('.message');
+const finalMessage = document.querySelector('.final-message');
+const form = document.querySelector('form');
 
-function submitForm() {
+form.onsubmit = (e) => {
+    e.preventDefault();
     const userName = document.getElementById("name").value;
     const userEmail = document.getElementById("email").value;
     console.log(userName + " " + userEmail);
     cardContainer.style.display = "none";
+    messageContainer.style.display = "block";
+    finalMessage.textContent = userName + ", thank you for your details. We will be in touch via " + userEmail + " shortly.";
 }
 
