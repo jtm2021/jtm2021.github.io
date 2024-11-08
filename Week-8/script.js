04b98525d1b687095bd22ece068d2330c6a05a7d
Jesson Mangilisan <jesson_chubz@yahoo.com.ph>
2024-11-08 22:14:06 +0000

    Add event listeners to button options

--- Week-8/script.js
+++ Week-8/script.js
@@ -1,5 +1,20 @@
 const startGameBtn = document.getElementById("start-game");
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
 
 startGameBtn.addEventListener('click', () => {
     document.querySelector('.intro').style.display = 'none';
 });
+
+rockBtn.addEventListener('click', () => {
+    console.log("rock");
+})
+
+paperBtn.addEventListener('click', () => {
+    console.log("paper");
+})
+
+scissorsBtn.addEventListener('click', () => {
+    console.log("scissors");
+})
