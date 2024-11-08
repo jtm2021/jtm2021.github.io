caee50d5974fc80ac24338dbbbe6a03bc99dd619
Jesson Mangilisan <jesson_chubz@yahoo.com.ph>
2024-11-08 22:38:07 +0000

    Add gameplay logic

--- Week-8/script.js
+++ Week-8/script.js
@@ -6,16 +6,41 @@ const scissorsBtn = document.getElementById("scissors");
 startGameBtn.addEventListener('click', () => {
     document.querySelector('.intro').style.display = 'none';
     document.querySelector('.game-section').style.display = 'flex';
+    let answer = computerBet();
+    console.log(answer);
 });
 
 rockBtn.addEventListener('click', () => {
-    console.log("rock");
+    startGame("rock");
 })
 
 paperBtn.addEventListener('click', () => {
-    console.log("paper");
-})
+    startGame("paper");
+});
 
 scissorsBtn.addEventListener('click', () => {
-    console.log("scissors");
+    startGame("scissors");
 })
+
+function computerBet() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    if (randomNumber === 1) {
+        return "rock";
+    } else if (randomNumber === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function startGame(userBet) {
+    const computerBet = this.computerBet();
+
+    if (userBet === computerBet) {
+        console.log("It's a tie!");
+    } else if ((userBet === "scissors" && computerBet === "paper") || (userBet === "scissors" && computerBet === "paper") || (userBet === "scissors" && computerBet === "paper")) {
+        console.log("You win!");
+    } else {
+        console.log("Haha computer wins!")
+    }
+}
