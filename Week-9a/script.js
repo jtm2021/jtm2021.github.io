d6556dc8fdffb3b9e3ddb7efb0f2bc4ae44bbbf0
Jesson Mangilisan <jesson_chubz@yahoo.com.ph>
2024-11-13 20:08:11 +0000

    Add javascript logic to display lyrics

---
+++ Week-9a/script.js
@@ -0,0 +1,22 @@
+function formulateLyrics() {
+    let bottlesOfBeer = 99;
+    let lyrics = "";
+    for (let i = bottlesOfBeer; i > 0; i--) {
+        if (i === 1) {
+            lyrics += i + "bottle of beer on the wall, " + i + "bottle of beer\n Take one down and pass it around, no more bottles of beer on the wall\n\n";
+        } else {
+            lyrics += i + "bottles of beer on the wall, " + i + "bottles of beer\n Take one down and pass it around," + + (i - 1) + (i - 1 === 1 ? "bottle" : "bottles") + "no more bottles of beer on the wall\n\n";
+        }
+    }
+
+    lyrics += "No more bottles of beer on the wall, no more bottles of beer.\n" +
+        "Go to the store and buy some more, 99 bottles of beer on the wall."
+
+    return lyrics;
+}
+
+function displayLyrics() {
+    let songLyrics =  this.formulateLyrics();
+    const lyrics = document.getElementById("lyrics");
+    lyrics.innerText = songLyrics;
+}
