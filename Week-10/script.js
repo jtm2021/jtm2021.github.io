27bbdfd5c6dc53ce5d9590e93850f9d2c9375b34
Jesson Mangilisan <jesson_chubz@yahoo.com.ph>
2024-11-24 12:30:24 +0000

    Add clicking function to button

--- Week-10/script.js
+++ Week-10/script.js
@@ -1,3 +1,6 @@
 const groceryInput = document.getElementById('grocery-input');
 const addGroceryItemBtn = document.getElementById('add-grocery-item');
 
+addGroceryItemBtn.addEventListener('click', () => {
+    console.log("hello");
+})
