87b759b41edf43d340fc06a689595d58592f36e6
Jesson Mangilisan <jesson_chubz@yahoo.com.ph>
2024-11-24 12:37:09 +0000

    Add basic logic to add item to grocery list

--- Week-10/script.js
+++ Week-10/script.js
@@ -1,6 +1,15 @@
 const groceryInput = document.getElementById('grocery-input');
 const addGroceryItemBtn = document.getElementById('add-grocery-item');
+const groceryList = document.getElementById('grocery-list');
 
 addGroceryItemBtn.addEventListener('click', () => {
-    console.log("hello");
+    addGroceryItem();
 })
+
+function addGroceryItem() {
+    const userGroceryInput = groceryInput.value;
+    const groceryItem = document.createElement('li');
+    groceryItem.textContent = userGroceryInput;
+
+    groceryList.appendChild(groceryItem);
+}
